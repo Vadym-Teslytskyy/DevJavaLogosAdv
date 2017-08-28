2e36f8fc18d68cd0812bd91699ef20de56459b72
Vadym <t.vadyk@gmail.com>
2017-08-28 16:20:11 +0300

    Add UserConverter <String, User>

---
+++ Dev_Java_Spring_Boot_Restaurant/src/main/java/ua/converter/UserConverter.java
@@ -0,0 +1,28 @@
+package ua.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import ua.entity.User;
+import ua.repository.UserRepository;
+
+@Component
+public class UserConverter implements Converter<String, User>{
+	
+	private final UserRepository repository;
+	
+	public UserConverter(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public User convert(String arg0) {
+		String email = SecurityContextHolder.getContext().getAuthentication().getName();
+		if(!email.equals("anonymousUser")){
+						return repository.findByEmail(email);
+					}
+		return null;
+	}
+
+}
