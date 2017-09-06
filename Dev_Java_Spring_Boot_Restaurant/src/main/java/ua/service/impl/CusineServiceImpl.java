a4a647cc43dfb3e5e3a6a3efe13ab106827499d0
Vadym <t.vadyk@gmail.com>
2017-09-06 17:19:41 +0300

    Посторіночна розбивка для Cusine(admin adding)

--- Dev_Java_Spring_Boot_Restaurant/src/main/java/ua/service/impl/CusineServiceImpl.java
+++ Dev_Java_Spring_Boot_Restaurant/src/main/java/ua/service/impl/CusineServiceImpl.java
@@ -2,6 +2,9 @@ package ua.service.impl;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import ua.entity.Cuisine;
@@ -10,10 +13,23 @@ import ua.service.CusineService;
 
 @Service
 public class CusineServiceImpl extends CrudServiceImpl<Cuisine, Integer> implements CusineService{
+	
+	private final CusineRepository reposirory;
+	
+	public CusineServiceImpl(JpaRepository<Cuisine, Integer> repository, CusineRepository reposirory) {
+		super(repository);
+		this.reposirory = reposirory;
+	}
 
 	@Autowired
 	public CusineServiceImpl(CusineRepository repository) {
 		super(repository);
+		this.reposirory = repository;
+	}
+
+	@Override
+	public Page<Cuisine> findAll(Pageable pageable) {
+		return reposirory.findAll(pageable);
 	}
 
 }
