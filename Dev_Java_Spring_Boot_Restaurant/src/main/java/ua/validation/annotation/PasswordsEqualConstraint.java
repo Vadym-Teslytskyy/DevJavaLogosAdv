d4c18c9a6547ec49981620048ddf75e0921af3f2
Vadym <t.vadyk@gmail.com>
2017-09-07 23:29:26 +0300

    Signed-off-by: Vadym <t.vadyk@gmail.com>

--- Dev_Java_Spring_Boot_Restaurant/src/main/java/ua/validation/annotation/PasswordsEqualConstraint.java
+++ Dev_Java_Spring_Boot_Restaurant/src/main/java/ua/validation/annotation/PasswordsEqualConstraint.java
@@ -2,7 +2,6 @@ package ua.validation.annotation;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 
 import javax.validation.Constraint;
@@ -11,7 +10,6 @@ import javax.validation.Payload;
 import ua.validation.validator.PasswordsEqualConstraintValidator;
 
 @Retention(RUNTIME)
-@Documented
 @Constraint(validatedBy = PasswordsEqualConstraintValidator.class)
 public @interface PasswordsEqualConstraint {
 	
